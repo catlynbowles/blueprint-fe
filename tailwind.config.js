34933fe0c2dd6f5d1fac8191944687e9ae2e0213
Catlyn Bowles <bowlescatlyn@gmail.com>
2023-05-31 08:31:12 -0600

    add loading spinner

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,9 +18,6 @@ module.exports = {
     fontFamily: {
       lato: ["Lato", "sans-serif"],
     },
-    animation: {
-      "fade-in": "pulse 2s cubic-bezier(0.4, 0, 0.6, 1)",
-    },
   },
   plugins: [],
 };
