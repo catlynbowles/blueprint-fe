943e34fe1ed9f240d4bedc56979d49d04913e7b4
Catlyn Bowles <bowlescatlyn@gmail.com>
2023-05-30 20:17:04 -0600

    add logo

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+import logo from "../../../public/images/logo.png";
 
 export default function Navbar() {
   return (
     <Link href="/">
-      <h1 className="p-5 text-2xl drop-shadow-md text-off-white font-extrabold">blueprint</h1>
+      <Image src={logo} height={25} width="auto" className="m-2" />
     </Link>
   );
 }
